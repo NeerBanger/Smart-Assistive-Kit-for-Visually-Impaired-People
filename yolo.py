8cf2055601833ebe93d4085dd3b372361a2cb79a
NeerBanger <37964323+NeerBanger@users.noreply.github.com>
2020-01-05 21:52:28 +0530

    Add files via upload

---
+++ yolo.py
@@ -0,0 +1,180 @@
+# USAGE
+# python yolo.py --image images/baggage_claim.jpg --yolo yolo-coco
+
+# import the necessary packages
+import numpy as np
+import argparse
+import time
+import os
+import cv2
+import pyttsx3
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("Smart Assistive System Image Detection")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("test", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "clicked_image.png"
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-y", "--yolo", required=True,
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# load our input image and grab its spatial dimensions
+image = cv2.imread(args["image"])
+(H, W) = image.shape[:2]
+
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# construct a blob from the input image and then perform a forward
+# pass of the YOLO object detector, giving us our bounding boxes and
+# associated probabilities
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+	swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+
+# show timing information on YOLO
+print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+# initialize our lists of detected bounding boxes, confidences, and
+# class IDs, respectively
+boxes = []
+confidences = []
+classIDs = []
+
+# loop over each of the layer outputs
+for output in layerOutputs:
+	# loop over each of the detections
+	for detection in output:
+		# extract the class ID and confidence (i.e., probability) of
+		# the current object detection
+		scores = detection[5:]
+		classID = np.argmax(scores)
+		confidence = scores[classID]
+
+		# filter out weak predictions by ensuring the detected
+		# probability is greater than the minimum probability
+		if confidence > args["confidence"]:
+			# scale the bounding box coordinates back relative to the
+			# size of the image, keeping in mind that YOLO actually
+			# returns the center (x, y)-coordinates of the bounding
+			# box followed by the boxes' width and height
+			box = detection[0:4] * np.array([W, H, W, H])
+			(centerX, centerY, width, height) = box.astype("int")
+
+			# use the center (x, y)-coordinates to derive the top and
+			# and left corner of the bounding box
+			x = int(centerX - (width / 2))
+			y = int(centerY - (height / 2))
+
+			# update our list of bounding box coordinates, confidences,
+			# and class IDs
+			boxes.append([x, y, int(width), int(height)])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+
+# apply non-maxima suppression to suppress weak, overlapping bounding
+# boxes
+idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+	args["threshold"])
+no=1
+result_output=[]
+file=open("opt.txt","w+")
+# ensure at least one detection exists
+if len(idxs) > 0:
+	# loop over the indexes we are keeping
+	for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            # draw a bounding box rectangle and label on the image
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            text1=LABELS[classIDs[i]]
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
+            result_output.append(text1)
+            for text1 in result_output:
+                np.savetxt("opt.txt", np.array(result_output), fmt="%s")
+#print(result_output)
+cv2.imwrite('output/output_image.png', image)
+engine = pyttsx3.init()
+sound = engine.getProperty('voices')
+rate = engine.getProperty('rate')
+volume = engine.getProperty('volume')
+print ("Sound = ",sound)
+print ("Rate = ",rate)
+print ("Volume = ",volume)
+engine.setProperty('rate' , rate-70)
+engine.setProperty('voice' ,sound[1].id)
+print("Opencv Version = ",cv2.__version__)
+          
+if result_output!=0:
+    contents=file.read()
+    engine.say("I can see")
+    engine.say(contents)
+    engine.runAndWait()
+file.close()
+# show the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+#python yolo.py --image clicked_image.png --yolo yolo-coco
+
+#$ python yolo.py --image images/soccer.jpg --yolo yolo-coco
+
+
+
+from tensorflow.python.client import device_lib 
+print(device_lib.list_local_devices())
